e4c8cd6ace279f23619da479f00792d7a0b4dd01
FCCHC <fccachon@gmail.com>
2017-10-16 15:59:38 -0500

    add CE,C functions to calculator

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import InputButton from './components/InputButton';
 
 //define the inputButtons that will be on the calculator
 const inputButtons = [
+  ['CE','C'],
   [1,2,3,'/'],
   [4,5,6,'*'],
   [7,8,9,'-'],
@@ -21,6 +22,17 @@ const inputButtons = [
 ];
 
 export default class ReactCalculator extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      previousInputValue: 0,
+      inputValue: 0,
+      selectedSymbol: null,
+      decimal: null
+    }
+  }
 
  _renderInputButtons(){
    let views =[];
@@ -32,7 +44,10 @@ export default class ReactCalculator extends Component<{}> {
      for(var i=0; i < row.length; i++){
        let input = row[i];
 
-       inputRow.push(<InputButton value={input} key={r + "-" + i}/>
+       inputRow.push(<InputButton value={input}
+                                  key={r + "-" + i}
+                                  highlight={this.state.selectedSymbol === input}
+                                  onPress={this._onInputButtonPressed.bind(this, input)}/>
       );
      }
       views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
@@ -41,13 +56,97 @@ export default class ReactCalculator extends Component<{}> {
    return views;
  }
 
+ _onInputButtonPressed(input){
+   switch (typeof input) {
+     case 'number':
+          return this._handleNumberInput(input)
+
+    case 'string':
+          return this._handleStringInput(input)
+   }
+ }
+
+ _handleNumberInput(num) {
+   let inputValue = (this.state.inputValue * 10) + num;
+
+   this.setState({
+        inputValue: inputValue
+   })
+ }
+
+ _handleStringInput(str){
+   switch (str) {
+     case '/':
+     case '*':
+     case '+':
+     case '-':
+         this.setState({
+           selectedSymbol: str,
+           previousInputValue: this.state.inputValue,
+           inputValue: 0
+         });
+         break;
+
+     case '=':
+         let symbol = this.state.selectedSymbol,
+             inputValue = this.state.inputValue,
+             previousInputValue = this.state.previousInputValue;
+
+          if(!symbol){
+             return;
+          }
+
+          this.setState({
+               previousInputValue: 0,
+               inputValue: eval(previousInputValue + symbol + inputValue),
+               selectedSymbol: null
+          });
+
+          break;
+
+      case '.':
+
+           this.setState({
+             decimal: true,
+             selectedSymbol: str,
+             previousInputValue:this.state.inputValue
+           })
+
+           break;
+
+      case 'CE':
+           this.setState({
+             previousInputValue: 0,
+             inputValue: 0,
+             selectedSymbol: null,
+             decimal: null
+           })
+          break;
+
+      case 'C':
+               let input = this.state.inputValue
+                   previous = this.state.previousInputValue
+                   str = this.state.selectedSymbol
+          this.setState({
+             inputValue:previous,
+             previousInputValue: input,
+             selectedSymbol: str
+          })
+
+       break;
+
+   }
+ }
+
   render() {
     return (
       <View style={Style.rootContainer}>
-        <View style={Style.displayContainer}></View>
-        <View style={Style.inputContainer}>
+          <View style={Style.displayContainer}>
+            <Text style={Style.displayText}>{this.state.inputValue}</Text>
+          </View>
+          <View style={Style.inputContainer}>
           {this._renderInputButtons()}
-        </View>
+          </View>
       </View>
     );
   }
--- src/components/InputButton.js
+++ src/components/InputButton.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, TouchableHighlight} from 'react-native';
 
 import Style from '/home/creatur/Documentos/react-native/ReactCalculator/src/StyleSheet.js';
 
 export default class InputButton extends Component {
    render() {
      return(
-     <View style={Style.inputButton}>
+     <TouchableHighlight style={[Style.inputButton, this.props.highlight ? Style.inputButtonHighlighted : null]}
+                         underlayColor='#193441'
+                         onPress={this.props.onPress}>
         <Text style={Style.inputButtonText}>{this.props.value}</Text>
-     </View>
+     </TouchableHighlight>
     )
    }
 }
